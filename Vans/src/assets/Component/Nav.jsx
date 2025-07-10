51c6d6e8c1a7c12fd0d6dec021806acc199b8951
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-07-09 22:07:00 -0400

    header

--- Vans/src/assets/Component/Nav.jsx
+++ Vans/src/assets/Component/Nav.jsx
@@ -1,8 +1,27 @@
+import VansLogo from '../images/VansLogo.png'
+import {navLinks} from '../constants/index'
+
 function Nav(){
   return(
-    <div className="text-slate-700">
-          La vie est belle
-    </div>
+    <header className=' absolute z-10 w-full'>
+           <nav className='flex justify-between items-center
+           max-container'>
+            <a href="#"> 
+              <img className='h-[70px] w-auto' src={VansLogo} alt="" />
+            </a>
+            <ul className=' flex justify-center items-center gap-16 w-fit'>
+                 {navLinks.map((item) =>(
+                  <li key={item.label}>
+                      <a href={item.href} className='text-lg text-slate-600' >
+                          {item.label}
+                      </a>
+                  </li>
+                 ))
+
+                 }
+            </ul>
+            </nav>
+    </header>
   )
 }
-export default Nav
+export default Nav 
