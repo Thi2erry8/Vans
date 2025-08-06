3e7346495e668c26ed220e0a341ff4657a5b6330
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-06 15:01:26 -0400

    Services_Start

--- Vans/src/assets/Component/ServiceCard.jsx
+++ Vans/src/assets/Component/ServiceCard.jsx
@@ -7,3 +7,4 @@ function ServiceCard (iconName , title , text){
         </div>
     )
 }
+export default ServiceCard
--- Vans/src/section/Services.jsx
+++ Vans/src/section/Services.jsx
@@ -1,4 +1,32 @@
+import ServiceCard from "../assets/Component/ServiceCard"
 function Services(){
     
+    const serviceDetails = [
+                               {
+                                iconName : "ri-flight-takeoff-line" ,
+                                title : "Free Shipping",
+                                text : "enjoy seamless shooping with our complimentary shipping service"
+                               },
+
+                               {
+                                iconName : "ri-shield-cross-line" ,
+                                title : "Secure Payment",
+                                text : "Experience worry-free transactions with our secure payments options"
+                               },
+
+                               {
+                                iconName : "ri-hand-heart-fill" ,
+                                title : "Love to help you",
+                                text : "Our dedicated team is here to assist you every step of the way"
+                               }
+                           ]
+
+
+
+    return(
+        <div className="min-h-screen w-full">
+              
+        </div>
+    )
 }
 export default Services
