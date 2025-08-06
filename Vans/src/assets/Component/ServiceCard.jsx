2f270fa845a509a2095ba623ac0b9fc2a5b86bd1
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-06 14:43:57 -0400

    Service Card

---
+++ Vans/src/assets/Component/ServiceCard.jsx
@@ -0,0 +1,9 @@
+function ServiceCard (iconName , title , text){
+    return(
+        <div className="flex flex-col">
+             <span className=""><i className={iconName} ></i></span> 
+              <h2 className="">{title}</h2>
+              <p className="">{text}</p>
+        </div>
+    )
+}
