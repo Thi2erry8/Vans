a3c3f0d6ef2b39c0d0f3923bfcce64c88d5cd99e
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-08 16:57:41 -0400

    Services_End

--- Vans/src/assets/Component/ServiceCard.jsx
+++ Vans/src/assets/Component/ServiceCard.jsx
@@ -1,9 +1,9 @@
 function ServiceCard( {iconName , title , text} ){
     return(
-        <div  className="flex flex-col w-65.5 h-65.5 items-start justify-center border border-red-400 p-3 rounded-2xl">
-             <span className=""><i className={iconName} ></i></span> 
-              <h2 className="">{title}</h2>
-              <p className="">{text}</p>
+        <div  className="flex flex-col w-65.5 h-65.5 items-start justify-center shadow-xl p-3 rounded-2xl gap-4">
+             <span className="rounded-2xl bg-red-500 px-2 py-0.5 text-lg text-white"><i className={iconName} ></i></span> 
+              <h2 className="font-bold text-lg">{title}</h2>
+              <p className="text-gray-500">{text}</p>
         </div>
     )
 }
