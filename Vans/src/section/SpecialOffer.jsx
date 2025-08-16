9d5bf757b0d7a041723243787d1f9667d22afd66
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-16 19:01:20 -0400

    Special_Offer_right_side_start

--- Vans/src/section/SpecialOffer.jsx
+++ Vans/src/section/SpecialOffer.jsx
@@ -11,12 +11,25 @@ function SpecialOffer(){
                               style={{backgroundImage: `url(${Vans1})`}}></div>
                               <div className="col-span-1 row-span-2 bg-gray-700 rounded-2xl text-white bg-cover bg-bottom"
                               style={{backgroundImage: `url(${Vans2})`}}></div>
-                              <div className="col-span-2 row-span-3 bg-gray-700 rounded-2xl text-white bg-cover bg-center"
+                              <div className="col-span-2 row-span-3 bg-gray-700 rounded-2xl text-white bg-cover bg-[position:20%_60%]"
                               style={{backgroundImage: `url(${Vans3})`}}></div>
                         </div>
                 </div>
-                <div className="h-screen flex flex-row w-full">
-                      
+                <div className=" flex flex-col items-center justify-center h-1/2 w-1/2">
+                      <h2><span>Special</span> Offer</h2>
+                     
+                      <p>Embark on a shopping journey that redefines your experiece
+                         with unbeateble deals. From premier selections to incredible save
+                         we offer unparalled value that set us apart
+                      </p>
+
+                      <p>Navigate a realm of possibilities designed to fullfil your uniques desires,
+                        surpassing the lofties expectations. Your journey with us is nothing short of exceptional
+                      </p>
+                      <div className="flex flex-row">
+                            
+                            <button>Learn more</button>
+                      </div>
                 </div>
             </div>
         )
