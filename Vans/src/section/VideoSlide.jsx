883f98c417a92e3713cb83f96ea8f9d6364dbae7
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-27 07:50:58 -0400

    000111

--- Vans/src/section/VideoSlide.jsx
+++ Vans/src/section/VideoSlide.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import Background from "../assets/Component/Background"
 function VideoSlide(){
-      return(
+      let heroData = [
+          {text1:'Stylish sneakers' , text2:'with recycled materials' },
+          {text1:'richly layered design' , text2:'pairs airy textiles' },
+          {text1:'Authentic style' , text2:'for every day' }
+        ]
+        const [heroCount, setHeroCount] = useState(2);
+        const [playStatus,setplayStatus] = useState(false)
+  return(
+       
         <section className="w-full h-screen">
-                  
+                  <Background playStatus={playStatus} heroCount={heroCount}/>
         </section>
       )
 }
