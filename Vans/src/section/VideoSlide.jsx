740175d78ef98db663df04f75381b5bfc5445622
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-08-26 23:20:29 -0400

    010110

--- Vans/src/section/VideoSlide.jsx
+++ Vans/src/section/VideoSlide.jsx
@@ -1,3 +1,7 @@
+import Video from '../assets/images/Video.webm'
+import Vans1 from '../assets/images/Vans_4.avif';
+import Vans2 from '../assets/images/Vans_5.avif';
+import Vans3 from '../assets/images/Vans_6.avif';
 function VideoSlide(){
       return(
         <section className="w-full h-screen">
