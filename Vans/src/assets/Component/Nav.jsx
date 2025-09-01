dc657aef51f72d9ad92dd426d5bc33618835ba10
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-09-01 18:44:31 -0400

    Responsive_start

--- Vans/src/assets/Component/Nav.jsx
+++ Vans/src/assets/Component/Nav.jsx
@@ -4,12 +4,11 @@ import {navLinks} from '../constants/index'
 function Nav(){
   return(
     <header className='bg-white/45  z-15 w-full px-4 fixed'>
-           <nav className='flex justify-between items-center
-           max-container'>
+           <nav className='flex justify-between items-center relative max-container'>
             <a href="#"> 
               <img className='h-[70px] w-auto' src={VansLogo} alt="" />
             </a>
-            <ul className=' flex justify-center items-center gap-16 w-fit max-lg:hidden'>
+            <ul className=' flex justify-center items-center gap-16 w-fit'>
                  {navLinks.map((item) =>(
                   <li key={item.label}>
                       <a href={item.href} className='text-lg text-slate-900 hover:text-slate-600 ease-in-out' >
@@ -20,7 +19,7 @@ function Nav(){
 
                  }
             </ul>
-            <div className='hidden max-lg:block'>
+            <div className='menu_btn'>
                   <i className="ri-menu-line text-3xl cursor-pointer "></i>
             </div>
             </nav>
--- Vans/src/section/Hero.jsx
+++ Vans/src/section/Hero.jsx
@@ -32,7 +32,7 @@ function Hero(){
 
  
       return (
-    <section id="home" className="w-full flex /* xl:flex-row */ flex-col justify-center items-center min-h-screen gap-10 max-container px-5">
+    <section id="home" className="w-full flex  flex-row justify-center items-center min-h-screen gap-10 max-container px-5">
               <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
                                <p className='text-lg text-red-600'>
                                 Ours Best Rated
